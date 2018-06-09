ec289150a22c7cf04b2dcbd074b6127a3b8d8151
Camille Carrier <camille.carrier@outlook.com>
2018-06-09 18:01:47 +0200

    Migration to Webpack build and packaging

---
+++ webpack.config.js
@@ -0,0 +1,92 @@
+// `CheckerPlugin` is optional. Use it if you want async error reporting.
+// We need this plugin to detect a `--watch` mode. It may be removed later
+// after https://github.com/webpack/webpack/issues/3460 will be resolved.
+const webpack = require('webpack');
+const { CheckerPlugin } = require('awesome-typescript-loader')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const helpers = require('./config/helpers');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    'polyfills': './src/polyfills.ts',
+    'app': './src/main.ts'
+  },
+
+  // Currently we need to add '.ts' to the resolve.extensions array.
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+
+  // Source maps support ('inline-source-map' also works)
+  devtool: 'source-map',
+
+  // Add the loader for .ts files.
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        enforce: 'pre',
+        loader: 'tslint-loader',
+        options: {
+          emitErrors: true,
+          failOnHint: true,
+          formatter: "stylish",
+        }
+      },
+      {
+        test: /\.tsx?$/,
+        loaders: ['awesome-typescript-loader', 'angular2-template-loader']
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+        loader: 'file-loader?name=assets/[name].[hash].[ext]'
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      }
+    ],
+  },
+  plugins: [
+    // Workaround for angular/angular#11580
+    new webpack.ContextReplacementPlugin(
+      // The (\\|\/) piece accounts for path separators in *nix and Windows
+      /angular(\\|\/)core(\\|\/)@angular/,
+      helpers.root('./src'), // location of your src
+      {} // a map of your routes
+    ),
+    new CheckerPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
+
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all"
+        }
+      }
+    }
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[hash].js',
+    chunkFilename: '[id].[hash].chunk.js'
+  }
+};
