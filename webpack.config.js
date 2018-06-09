0c3687f1c5e28e8d1d0588dfd75eb7ee1fde16b0
Alexandre Soro <alexandre@soro.email>
2018-06-10 00:08:26 +0200

    Add favicon management

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const { CheckerPlugin } = require('awesome-typescript-loader')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 const path = require('path');
 const helpers = require('./config/helpers');
 
@@ -62,6 +63,7 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
+    new FaviconsWebpackPlugin('./src/favicon.png'),
     // Workaround for angular/angular#11580
     new webpack.ContextReplacementPlugin(
       // The (\\|\/) piece accounts for path separators in *nix and Windows
