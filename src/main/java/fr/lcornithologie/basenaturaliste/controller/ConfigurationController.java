14b89a60f65d33bb0c08ef8bc91a879fe13a1a26
Camille Carrier <camille.carrier@outlook.com>
2018-06-09 13:54:47 +0200

    fix warnings

--- src/main/java/fr/lcornithologie/basenaturaliste/controller/ConfigurationController.java
+++ src/main/java/fr/lcornithologie/basenaturaliste/controller/ConfigurationController.java
@@ -1,24 +1,18 @@
 package fr.lcornithologie.basenaturaliste.controller;
 
 import java.net.URISyntaxException;
-import java.util.List;
 import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import fr.lcornithologie.basenaturaliste.model.AppConfiguration;
-import fr.lcornithologie.basenaturaliste.model.Sexe;
 import fr.lcornithologie.basenaturaliste.page.ConfigurationPage;
-import fr.lcornithologie.basenaturaliste.page.EntiteResult;
 import fr.lcornithologie.basenaturaliste.service.ConfigurationService;
-import fr.lcornithologie.basenaturaliste.service.SexeService;
 
 @RestController
 @RequestMapping("/api/configuration")
