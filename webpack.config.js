a2c21c639ed389a193a4d91351dbcec48461eb83
Alexandre Soro <alexandre@soro.email>
2018-07-07 10:30:36 +0200

    Migrate to webpack-serve

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,37 @@
 // `CheckerPlugin` is optional. Use it if you want async error reporting.
 // We need this plugin to detect a `--watch` mode. It may be removed later
 // after https://github.com/webpack/webpack/issues/3460 will be resolved.
-const webpack = require('webpack');
-const { CheckerPlugin } = require('awesome-typescript-loader')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
+const { CheckerPlugin } = require("awesome-typescript-loader");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
-const path = require('path');
-const helpers = require('./config/helpers');
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
+const path = require("path");
+const helpers = require("./config/helpers");
+const history = require("connect-history-api-fallback");
+const convert = require("koa-connect");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
-    'polyfills': './src/polyfills.ts',
-    'app': './src/main.ts'
+    polyfills: "./src/polyfills.ts",
+    app: "./src/main.ts"
   },
 
   // Currently we need to add '.ts' to the resolve.extensions array.
   resolve: {
-    extensions: ['.ts', '.js']
+    extensions: [".ts", ".js"]
   },
 
   // Source maps support ('inline-source-map' also works)
-  devtool: 'source-map',
+  devtool: "source-map",
 
-  devServer: {
+  serve: {
     port: 3000,
-    historyApiFallback: true
+    add: (app, middleware, options) => {
+      app.use(convert(history({})));
+    }
   },
 
   // Add the loader for .ts files.
@@ -35,31 +39,33 @@ module.exports = {
     rules: [
       {
         test: /\.ts$/,
-        enforce: 'pre',
-        loader: 'tslint-loader',
+        enforce: "pre",
+        loader: "tslint-loader",
         options: {
           emitErrors: true,
           failOnHint: true,
-          formatter: "stylish",
+          formatter: "stylish"
         }
       },
       {
         test: /\.tsx?$/,
-        loaders: ['awesome-typescript-loader', 'angular2-template-loader']
+        loaders: ["awesome-typescript-loader", "angular2-template-loader"]
       },
       {
         test: /\.html$/,
-        loader: 'html-loader'
+        loader: "html-loader"
       },
       {
         test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-        loader: 'file-loader?name=assets/[name].[hash].[ext]'
+        loader: "file-loader?name=assets/[name].[hash].[ext]"
       },
       {
         test: /\.scss$/,
         use: [
           // fallback to style-loader in development
-          process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+          process.env.NODE_ENV !== "production"
+            ? "style-loader"
+            : MiniCssExtractPlugin.loader,
           "css-loader",
           "sass-loader"
         ]
@@ -68,26 +74,26 @@ module.exports = {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, "css-loader"]
       }
-    ],
+    ]
   },
   plugins: [
     new webpack.ProvidePlugin({
-      jQuery: 'jquery',
-      $: 'jquery',
-      jquery: 'jquery'
+      jQuery: "jquery",
+      $: "jquery",
+      jquery: "jquery"
     }),
-    new CleanWebpackPlugin(['dist']),
-    new FaviconsWebpackPlugin('./src/favicon.png'),
+    new CleanWebpackPlugin(["dist"]),
+    new FaviconsWebpackPlugin("./src/favicon.png"),
     // Workaround for angular/angular#11580
     new webpack.ContextReplacementPlugin(
       // The (\\|\/) piece accounts for path separators in *nix and Windows
       /angular(\\|\/)core(\\|\/)@angular/,
-      helpers.root('./src'), // location of your src
+      helpers.root("./src"), // location of your src
       {} // a map of your routes
     ),
     new CheckerPlugin(),
     new HtmlWebpackPlugin({
-      template: 'src/index.html'
+      template: "src/index.html"
     }),
     new MiniCssExtractPlugin({
       filename: "[name].css",
@@ -108,8 +114,8 @@ module.exports = {
   },
 
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[hash].js',
-    chunkFilename: '[id].[hash].chunk.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[hash].js",
+    chunkFilename: "[id].[hash].chunk.js"
   }
 };
