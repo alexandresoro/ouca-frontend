4fc1a7b4f95d06c0cbe0649871642c925e5dac56
Camille CARRIER <camille.carrier@amadeus.com>
2018-07-16 13:14:29 +0200

    departement form

--- src/app/pages/entities/departement/departement-form.component.ts
+++ src/app/pages/entities/departement/departement-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { FormControl, Validators } from "@angular/forms";
 import { Departement } from "../../../model/departement.object";
 import { EntiteSimpleFormComponent } from "../entite-simple/entite-simple-form.component";
 
@@ -7,6 +8,8 @@ import { EntiteSimpleFormComponent } from "../entite-simple/entite-simple-form.c
     templateUrl: "./departement-form.tpl.html"
 })
 export class DepartementFormComponent extends EntiteSimpleFormComponent<Departement> {
+
+    public codeFormControl = new FormControl("", [Validators.required]);
 
     getNewObject(): Departement {
         return new Departement();
