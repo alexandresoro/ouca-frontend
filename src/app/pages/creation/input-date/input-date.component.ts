1da9d3e13e49917a448514009b592b292e519736
Camille Carrier <camille@OrdinateCamille.lan>
2019-03-03 12:25:18 +0100

    update components with form controls

--- src/app/pages/creation/input-date/input-date.component.ts
+++ src/app/pages/creation/input-date/input-date.component.ts
@@ -1,12 +1,11 @@
-import { Component, Input } from "@angular/core";
+import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
 import { FormControl } from "@angular/forms";
 
 @Component({
   selector: "input-date",
-  templateUrl: "./input-date.tpl.html"
+  templateUrl: "./input-date.tpl.html",
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class InputDateComponent {
-  @Input() public selectedDate: string;
-
   @Input() public control: FormControl;
 }
