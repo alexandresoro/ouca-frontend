921e27c53c739df9cd0fab2388ec1e8f3dbb565b
Camille Carrier <camille@Ordinateur-de-Camille.local>
2019-05-07 13:20:46 +0200

    Create cross field error matcher

---
+++ src/app/modules/shared/matchers/cross-field-error.matcher.ts
@@ -0,0 +1,14 @@
+import { FormControl, FormGroupDirective, NgForm } from "@angular/forms";
+import { ErrorStateMatcher } from "@angular/material";
+
+export class CrossFieldErrorMatcher implements ErrorStateMatcher {
+  isErrorState(
+    control: FormControl,
+    form: FormGroupDirective | NgForm | null
+  ): boolean {
+    const invalidCtrl = !!(control && control.dirty && control.invalid);
+    const invalidParent = !!(form && form.dirty && form.invalid);
+
+    return invalidCtrl || invalidParent;
+  }
+}
