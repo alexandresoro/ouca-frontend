d921f579114f220876a702299906f2b460d2436a
Alexandre Soro <code@soro.email>
2019-06-13 20:03:27 +0200

    Update some packages

--- webpack.aot.config.js
+++ webpack.aot.config.js
@@ -1,7 +1,7 @@
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 const path = require("path");
 const helpers = require("./config/helpers");
