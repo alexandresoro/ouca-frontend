c7a448f001468d7696950d9144373f6b1cad9e83
Camille Carrier <camille.carrier@outlook.com>
2019-08-11 22:01:33 +0200

    Remove unused variable

--- src/app/modules/donnee-view/components/table-donnees/table-donnees.component.ts
+++ src/app/modules/donnee-view/components/table-donnees/table-donnees.component.ts
@@ -44,56 +44,6 @@ export class TableDonneesComponent {
     "observateur"
   ];
 
-  public displayedColumnsV2: string[] = [
-    "id",
-    "observateur",
-    "associes",
-    "date",
-    "heure",
-    "duree",
-    "departement",
-    "code_commune",
-    "nom_commune",
-    "lieudit",
-    "altitude",
-    "longitude",
-    "latitude",
-    "temperature",
-    "meteos",
-    "classe",
-    "code_espece",
-    "nom_francais",
-    "nom_latin",
-    "nombre",
-    "estimation_nombre",
-    "sexe",
-    "age",
-    "estimation_distance",
-    "distance",
-    "regroupement",
-    "code_comportement_1",
-    "libelle_comportement_1",
-    "code_comportement_2",
-    "libelle_comportement_2",
-    "code_comportement_3",
-    "libelle_comportement_3",
-    "code_comportement_4",
-    "libelle_comportement_4",
-    "code_comportement_5",
-    "libelle_comportement_5",
-    "code_comportement_6",
-    "libelle_comportement_6",
-    "code_milieu_1",
-    "libelle_milieu_1",
-    "code_milieu_2",
-    "libelle_milieu_2",
-    "code_milieu_3",
-    "libelle_milieu_3",
-    "code_milieu_4",
-    "libelle_milieu_4",
-    "commentaire"
-  ];
-
   @Input() public donneesToDisplay: any[];
 
   public dataSource: MatTableDataSource<any> = new MatTableDataSource();
