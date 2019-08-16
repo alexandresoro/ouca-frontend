fe1d8556f38206f72da2d9a35248937093229636
Camille Carrier <camille.carrier@outlook.com>
2019-08-16 10:36:46 +0200

    Create a shared component for multiple select

---
+++ src/app/modules/shared/components/form/multiple-select/multiple-select.component.ts
@@ -0,0 +1,36 @@
+import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
+import { FormControl } from "@angular/forms";
+import { EntiteSimple } from "basenaturaliste-model/entite-simple.object";
+
+@Component({
+  selector: "multiple-select",
+  templateUrl: "./multiple-select.tpl.html",
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class MultipleSelectComponent {
+  @Input() public attributesToDisplay: string[];
+  @Input() public control: FormControl;
+  @Input() public id: string;
+  @Input() public options: EntiteSimple[];
+  @Input() public placeholder: string;
+
+  public getDisplayedValue(object: EntiteSimple): string {
+    let displayedValue: string = "";
+
+    if (!!object) {
+      displayedValue = object[this.attributesToDisplay[0]];
+      for (
+        let indexAttribute = 1;
+        indexAttribute < this.attributesToDisplay.length;
+        indexAttribute++
+      ) {
+        displayedValue =
+          displayedValue +
+          " - " +
+          object[this.attributesToDisplay[indexAttribute]];
+      }
+    }
+
+    return displayedValue;
+  }
+}
