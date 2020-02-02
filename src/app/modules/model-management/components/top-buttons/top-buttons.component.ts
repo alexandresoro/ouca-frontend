296aaed73da929a07044bdac910c05da47a87c01
Camille Carrier <camille.carrier@outlook.com>
2020-02-02 10:11:57 +0100

    Move CSS at component level

--- src/app/modules/model-management/components/top-buttons/top-buttons.component.ts
+++ src/app/modules/model-management/components/top-buttons/top-buttons.component.ts
@@ -1,34 +1,34 @@
-import {Component, EventEmitter, Input, Output} from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 
 @Component({
-    selector: "top-buttons",
-    templateUrl: "./top-buttons.tpl.html",
+  selector: "top-buttons",
+  styleUrls: ["./top-buttons.component.scss"],
+  templateUrl: "./top-buttons.component.html"
 })
 export class TopButtonsComponent {
+  @Input() public title: string;
 
-    @Input() public title: string;
+  @Input() public createButtonLibelle: string = "Créer";
 
-    @Input() public createButtonLibelle: string = "Créer";
+  @Input() public exportButtonLibelle: string = "Exporter";
 
-    @Input() public exportButtonLibelle: string = "Exporter";
+  @Input() public areButtonsDisplayed: boolean;
 
-    @Input() public areButtonsDisplayed: boolean;
+  @Output() public onClickNewObject = new EventEmitter();
 
-    @Output() public onClickNewObject = new EventEmitter();
+  @Output() public onClickExportObjects = new EventEmitter();
 
-    @Output() public onClickExportObjects = new EventEmitter();
+  @Output() public onClickRefresh = new EventEmitter();
 
-    @Output() public onClickRefresh = new EventEmitter();
+  public newObject(): void {
+    this.onClickNewObject.emit();
+  }
 
-    public newObject(): void {
-        this.onClickNewObject.emit();
-    }
+  public exportObjects(): void {
+    this.onClickExportObjects.emit();
+  }
 
-    public exportObjects(): void {
-        this.onClickExportObjects.emit();
-    }
-
-    public refresh(): void {
-        this.onClickRefresh.emit();
-    }
+  public refresh(): void {
+    this.onClickRefresh.emit();
+  }
 }
