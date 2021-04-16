c73e38c3473e30c821501e44584d51f0575dba0c
Alexandre Soro <code@soro.email>
2021-04-16 21:20:29 +0200

    Upgrade version management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from "@angular/core";
+import { MatDialog } from '@angular/material/dialog';
+import { filter, map } from 'rxjs/operators';
 import { INIT } from './model/websocket/websocket-message-type.model';
+import { ApplicationUpgradeDialog } from './modules/application-management/components/application-upgrade-dialog/application-upgrade-dialog';
 import { AppConfigurationService } from "./services/app-configuration.service";
 import { BackendWsService } from "./services/backend-ws.service";
 import { EntitiesStoreService } from "./services/entities-store.service";
@@ -13,12 +16,28 @@ export class AppComponent implements OnInit {
   constructor(
     private backendWsService: BackendWsService,
     private appConfigurationService: AppConfigurationService,
-    private entitiesStoreService: EntitiesStoreService
+    private entitiesStoreService: EntitiesStoreService,
+    private dialog: MatDialog
   ) { }
 
   ngOnInit(): void {
     this.appConfigurationService.initializeConfigurationStore();
     this.entitiesStoreService.initializeEntitiesStore();
+    this.backendWsService.getUpdateMessageContent$().pipe(
+      filter(updateMessage => !!(updateMessage?.version)),
+      map(updateMessage => updateMessage.version)
+    ).subscribe((appVersion) => {
+      // Check that the database and app versions are matching
+      // If it is the case, do nothing, otherwise, display the migration dialog
+      if (appVersion.application > appVersion.database) {
+        this.dialog.open(ApplicationUpgradeDialog, {
+          data: appVersion,
+          width: "800px",
+          hasBackdrop: true,
+          disableClose: true
+        });
+      }
+    });
     this.backendWsService.sendMessage({ type: INIT });
   }
 }
