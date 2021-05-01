5a3696d8463f3ed4a6ece5e58e376cf5a0d76ef0
Alexandre Soro <code@soro.email>
2021-05-01 15:52:29 +0200

    Refactor autocomplete component

--- src/app/modules/shared/components/autocomplete/autocomplete.component.ts
+++ src/app/modules/shared/components/autocomplete/autocomplete.component.ts
@@ -1,13 +1,24 @@
 import {
+  AfterViewInit,
   ChangeDetectionStrategy,
   Component,
+  ElementRef,
+
+  EventEmitter,
+
   Input,
-  OnInit
+  OnDestroy,
+  OnInit,
+
+  Output,
+
+  ViewChild
 } from "@angular/core";
 import { FormControl } from "@angular/forms";
+import { MatAutocomplete, MatAutocompleteActivatedEvent } from '@angular/material/autocomplete';
 import deburr from 'lodash.deburr';
-import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
+import { fromEvent, Observable, of, Subject } from "rxjs";
+import { filter, map, startWith, takeUntil, withLatestFrom } from "rxjs/operators";
 import { EntiteSimple } from 'src/app/model/types/entite-simple.object';
 import { sortBy } from '../../helpers/utils';
 import { AutocompleteAttribute } from "./autocomplete-attribute.object";
@@ -17,10 +28,17 @@ import { AutocompleteAttribute } from "./autocomplete-attribute.object";
   templateUrl: "./autocomplete.tpl.html",
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AutocompleteComponent implements OnInit {
+export class AutocompleteComponent<T extends EntiteSimple> implements OnInit, AfterViewInit, OnDestroy {
+
+  @ViewChild("autocompleteInput") autocompleteInput: ElementRef<HTMLInputElement>;
+
+  @ViewChild("auto") autocomplete: MatAutocomplete;
+
   @Input() public type: string;
 
   @Input() public values: EntiteSimple[];
+
+  @Input() public valuesObs?: Observable<EntiteSimple[]>;
 
   @Input() public attributesToFilter: AutocompleteAttribute[];
 
@@ -28,24 +46,101 @@ export class AutocompleteComponent implements OnInit {
 
   @Input() public displayFn: ((value: any) => string) | null;
 
+  @Output() public optionActivated: EventEmitter<T> = new EventEmitter<T>();
+
+  @Output() public focus: EventEmitter<Event> = new EventEmitter<Event>();
+
+  @Output() public focusout: EventEmitter<Event> = new EventEmitter<Event>();
+
+  private readonly destroy$ = new Subject();
+
   filteredValues: Observable<EntiteSimple[]>;
+
+  private optionActivated$ = new Subject<T>();
 
   private CHARACTERS_TO_IGNORE = /(\s|\'|\-|\,)/g;
 
   ngOnInit(): void {
     this.filteredValues = this.control.valueChanges.pipe(
-      map(value => {
-        return typeof value === "string" ||
-          typeof value === "undefined" ||
-          value === null
-          ? value
-          : "" + value[this.attributesToFilter[0].key];
-      }),
-      map(value => (value ? this._filter(value) : []))
+      withLatestFrom(this.valuesObs ?? of(this.values)),
+      map(([value, allValues]) => {
+        if ((typeof value === "string") || value == null) {
+          // If the value is a string or null, then look for entities that are matching
+          return this._filter(value);
+        } else {
+          // If not, we assume that it is an entite simple that is selected, so we only display this one as matching
+          const valueIdentifier = value[this.attributesToFilter[0].key];
+
+          const matchingEntity = allValues?.find((oneValue) => {
+            return oneValue[this.attributesToFilter[0].key] === valueIdentifier;
+          });
+
+          return matchingEntity ? [matchingEntity] : [];
+        }
+      })
     );
+
+
+    this.optionActivated$
+      .pipe(
+        takeUntil(this.destroy$)
+      )
+      .subscribe((optionActivated) => {
+        this.optionActivated?.emit(optionActivated);
+      });
+
+  }
+
+  ngAfterViewInit(): void {
+
+    fromEvent(this.autocompleteInput.nativeElement, "focus").subscribe((event) => {
+      this.focus?.emit(event);
+    })
+
+    fromEvent(this.autocompleteInput.nativeElement, "focusout") // focusout is the event triggered when the input is about to lose the focus
+      .pipe(
+        filter(() => !this.autocomplete.isOpen), // ignore focusout events that are because its own autocomplete panel has been opened
+        withLatestFrom(
+          this.optionActivated$.pipe(startWith(null as EntiteSimple)), // this is how we are detecting that we need to set an option
+          this.filteredValues
+        ),
+        takeUntil(this.destroy$)
+      ).subscribe(([event, optionActivated, filteredValues]) => {
+
+        this.focusout?.emit(event);
+
+        // Here we have the list of filtered values that correspond to the currently possible selections
+        // and the option selected that corresponds to the LATEST option that was active
+
+        // The case where the "option selected" is not included in the filtered values is the case where for instance
+        // we type a letter that opens the selection and will activate the first match
+        // then we clear the field => filteredValues will become empty
+        // In that case, the option is still "selected" but is not valid anymore
+
+        if (filteredValues.includes(optionActivated) && optionActivated !== this.control.value) {
+          this.control.setValue(optionActivated);
+        } if (!this.control.valid) {
+          this.control.setValue(null);
+        }
+      });
+
+  }
+
+  public ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  public onOptionActivated = (event: MatAutocompleteActivatedEvent): void => {
+    this.optionActivated$.next(event?.option?.value as T);
   }
 
   private _filter(value: string): EntiteSimple[] {
+
+    if (!value) {
+      return [];
+    }
+
     const filterValue: string = deburr(value.toLowerCase()).replace(
       this.CHARACTERS_TO_IGNORE,
       ""
